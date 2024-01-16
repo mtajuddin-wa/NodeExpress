ad501cbc960af1bef7108899ed80b129ec1e08ee
Taj <mt17bee0178@gmail.com>
2024-01-17 00:14:05 +0530

    Node

---
+++ Node/node1.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req,res) => {
+    switch(req.url) {
+        case "/users" :
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(fs.readFileSync('user.json', 'utf8'));
+            break;
+
+        case "/":
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/HTML');
+            res.end(fs.readFileSync('index.html', 'utf8'));
+            break;
+        default:
+            res.statusCode = 404;
+            res.end("Page Not Found!");
+    };
+}).listen(8080, 'localhost', () => {
+    console.log("SErver is running");
+});
